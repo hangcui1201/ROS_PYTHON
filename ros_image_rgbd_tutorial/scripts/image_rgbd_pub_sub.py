ace1a4f0500419310c41ba07d4032228e5e4ef38
Hang Cui <hangcui1201@gmail.com>
2019-08-23 11:44:05 -0500

    Add ros_image_rgbd_tutorial

---
+++ ros_image_rgbd_tutorial/scripts/image_rgbd_pub_sub.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+# $ roslaunch realsense_camera r200_nodelet_rgbd.launch
+# $ roslaunch ros_image_rgbd_tutorial depth_image_to_laser.launch  # /scan 
