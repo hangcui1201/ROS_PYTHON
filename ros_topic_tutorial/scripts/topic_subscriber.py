da57445df547353d2be43dfa501b62e6282bc341
Hang Cui <hangcui1201@gmail.com>
2019-08-28 15:21:19 -0500

    Update topic_subscriber.py

--- ros_topic_tutorial/scripts/topic_subscriber.py
+++ ros_topic_tutorial/scripts/topic_subscriber.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 import rospy
+import rosnode
 from std_msgs.msg import String
 from std_msgs.msg import Int32 
 from ros_topic_tutorial.msg import TopicMessage
 
 def msgCallback(msg):
+
+	ros_node_name = "ROS node: %s" % rosnode.get_node_names()
+	rospy.loginfo(ros_node_name)
+
+	# rospy.get_caller_id()
 
 	receive_secs = "recieve msg = %i" % msg.stamp.secs
 	rospy.loginfo(receive_secs)
@@ -16,23 +22,17 @@ def msgCallback(msg):
 	receive_count = "recieve msg = %i" % msg.count
 	rospy.loginfo(receive_count)
 
-	# rospy.get_caller_id()
-
 
 def listener():
 
-    # node name: topic_subscriber
-    # topic name: ros_msg_topic
-    # data type: String
-    rospy.init_node('topic_subscriber', anonymous=True)
-    rospy.Subscriber('ros_msg_topic', TopicMessage, msgCallback)
+	# node name: topic_subscriber
+	# topic name: ros_msg_topic
+	# data type: String
+	rospy.init_node('topic_subscriber', anonymous=True)
+	rospy.Subscriber('ros_msg_topic', TopicMessage, msgCallback)
 
-    # Keep the node from exiting until it is shutdown
-    rospy.spin()
+	# Keep the node from exiting until it is shutdown
+	rospy.spin()
 
 if __name__ == '__main__':
-    listener()
-
-
-
-
+	listener()
